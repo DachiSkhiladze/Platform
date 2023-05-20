8bfcebec6bf89a6f3ada60f068dd6e6dd0cb199e
DachiSkhiladze <dachiskhiladze@gmail.com>
2023-05-20 19:09:03 +0400

    Added http factory

---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,36 @@
+﻿using PlatformService.DTOs;
+using System.Text;
+using System.Text.Json;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpCommandDataClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task SendPlatformToCommand(PlatformReadDTO plat)
+        {
+            var httpContent = new StringContent(
+                JsonSerializer.Serialize(plat),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await _httpClient.PostAsync("https://localhost:6001/api/c/Platforms", httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("--> Sync POST to CommandSerivce was OK!");
+            }
+            else
+            {
+                Console.WriteLine("--> Sync POST to CommandSerivce was NOT OK!");
+            }
+        }
+    }
+}
---
+++ PlatformService/SyncDataServices/Http/ICommandDataClient.cs
@@ -0,0 +1,9 @@
+﻿using PlatformService.DTOs;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public interface ICommandDataClient
+    {
+        Task SendPlatformToCommand(PlatformReadDTO plat);
+    }
+}
