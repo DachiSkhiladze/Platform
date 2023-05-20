cca433e888487ee211a41dc5b4286995ce14c63e
DachiSkhiladze <dachiskhiladze@gmail.com>
2023-05-20 18:35:52 +0400

    Added new Controller

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # COmmand Service");
+
+            return Ok("Inbound test of from Platforms Controller");
+        }
+    }
+}
