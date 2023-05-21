0a049f3104c35b4f837c4fc5cf995534873bf1c8
DachiSkhiladze <dachiskhiladze@gmail.com>
2023-05-21 17:15:37 +0400

    Registered message bus client in di container and implemented couple more methods

--- PlatformService/AsyncDataServices/MessageBusClient.cs
+++ PlatformService/AsyncDataServices/MessageBusClient.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using PlatformService.DTOs;
 using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
 
 namespace PlatformService.AsyncDataServices
 {
@@ -37,7 +39,38 @@ namespace PlatformService.AsyncDataServices
 
         public void PublishNewPlatform(PlatformPublishDto platformPublishDto)
         {
+            var message = JsonSerializer.Serialize(platformPublishDto);
 
+            if (_connection.IsOpen)
+            {
+                Console.WriteLine("--> RabbitMQ Connection Open, sending message...");
+                SendMessage(message);
+            }
+            else
+            {
+                Console.WriteLine("--> RabbitMQ Connection Is Closed, not sending");
+            }
+        }
+
+        private void SendMessage(string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+
+            _channel.BasicPublish(exchange: "trigger",
+                                routingKey: "",
+                                basicProperties: null,
+                                body: body);
+            Console.WriteLine($"--> We have sent {message}");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("MessageBus Disposed");
+            if (_channel.IsOpen)
+            {
+                _channel.Close();
+                _connection.Close();
+            }
         }
 
         private void RabbitMQ_ConnectionShutDown(object sender, ShutdownEventArgs e)
