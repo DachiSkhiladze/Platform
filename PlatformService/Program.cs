16a275b6550109024077467756f6edc01a207d89
DachiSkhiladze <dachiskhiladze@gmail.com>
2023-05-24 00:24:10 +0400

    Added platforms proto file

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -24,7 +24,6 @@ builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
